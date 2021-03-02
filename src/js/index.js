e54c7171bdc58cababbc229d8f24105472498e36
Leonardo Espinoza <leoespinozidk@gmail.com>
2021-03-02 13:29:05 -0600

    adding js as

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,21 @@
-console.log("Initial");
+const allArrows = Array.from(document.querySelectorAll(".question span"));
+const allQuestions = Array.from(document.querySelectorAll(".question"));
+const allAnswers = Array.from(document.querySelectorAll(".answer"));
+
+const checkClassName = (element, className) => {
+  if (element.classList.contains(className)) {
+    element.classList.remove(className);
+  } else {
+    element.classList.add(className);
+  }
+};
+
+allQuestions.map((question, id) => {
+  const currentAnswer = allAnswers[id];
+  const currentArrow = allArrows[id];
+  question.addEventListener("click", () => {
+    checkClassName(question, "strong");
+    checkClassName(currentArrow, "rotate");
+    checkClassName(currentAnswer, "showAnswer");
+  });
+});
